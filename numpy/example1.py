c142a9a334af36b1916d4e4542b5b5598d58f8e9
Alexander <haska87@gmail.com>
2019-09-25 21:00:38 +0900

    Update example1.py

--- numpy/example1.py
+++ numpy/example1.py
@@ -4,7 +4,7 @@ import numpy as np
 # Создание массива
 a = np.array([1, 2, 3]) #одномерный
 print(a)
-print(type(a))
+print(type(a)) #тип
 
 b = np.array([[1, 2, 3], [1.5, 2.5, 3.5]]) #двумерный
 print(b)
@@ -22,7 +22,7 @@ print(e)
 
 print(np.empty((3, 3))) #пустой
 
-print(np.arange(0, 20, 5)) #интервал с шагом
+print(np.arange(0, 21, 5)) #интервал с шагом
 print(np.arange(1, 2, 0.1))
 
-print(np.linspace(1, 10, 10)) #интервал с количеством
+print(np.linspace(1, 9, 10)) #интервал с количеством
