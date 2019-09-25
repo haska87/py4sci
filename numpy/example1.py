784be5eaa6247be8361d742b618922b3f30a9f2e
Alexander <haska87@gmail.com>
2019-09-25 16:03:30 +0900

    Add files via upload

--- numpy/example1.py
+++ numpy/example1.py
@@ -6,23 +6,23 @@ a = np.array([1, 2, 3]) #одномерный
 print(a)
 print(type(a))
 
-b = np.array([[1.5, 2, 3], [4, 5, 6]]) #двумерный
+b = np.array([[1, 2, 3], [1.5, 2.5, 3.5]]) #двумерный
 print(b)
 
-b = np.array([[1.5, 2, 3], [4, 5, 6]], dtype=np.complex) #типизированный
+b = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.complex) #типизированный
 print(b)
 
 # Заполнение
-z = np.zeros((3, 5)) #нулями
+z = np.zeros((3, 3)) #нулями
 print(z)
 o = np.ones((2, 2, 2)) #единицами
 print(o)
-e = np.eye(5) #единичная матрица
+e = np.eye(4) #единичная матрица
 print(e)
 
 print(np.empty((3, 3))) #пустой
 
-print(np.arange(10, 30, 5)) #интервал с шагом
-print(np.arange(0, 1, 0.1))
+print(np.arange(0, 20, 5)) #интервал с шагом
+print(np.arange(1, 2, 0.1))
 
-print(np.linspace(0, 9, 10)) #интервал с количеством
+print(np.linspace(1, 10, 10)) #интервал с количеством
