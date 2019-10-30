8853fc31d8cf793fc4d566eb2bd6122e4bab3f3b
Alexander <haska87@gmail.com>
2019-10-30 16:20:38 +0900

    Add files via upload

---
+++ numpy/linalg.py
@@ -0,0 +1,35 @@
+# Documentation - https://docs.scipy.org/doc/numpy/reference/routines.linalg.html
+
+import numpy as np
+
+a = [[9,  1,  1],
+	 [1,  8,  1],
+	 [1,  1,  7]]
+
+n = 2
+print("Возводит матрицу в степень n:\n", np.linalg.matrix_power(a, n))
+
+print ("#Некоторые характеристики матриц#")
+print("Собственные значения и собственные векторы:\n", np.linalg.eig(a))
+
+print("Определитель:\n", np.linalg.det(a))
+
+print("Знак и логарифм определителя:\n", np.linalg.slogdet(a))
+
+print ("#Разложения#")
+print("Разложение Холецкого:\n", np.linalg.cholesky(a))
+
+print("QR разложение:\n", np.linalg.qr(a))
+
+print("Сингулярное разложение:\n", np.linalg.svd(a))
+
+print("#Системы уравнений#")
+
+b = [1, 2, 3]
+print("Решает систему линейных уравнений Ax = b:\n", np.linalg.solve(a, b)) 
+
+print("Решает тензорную систему линейных уравнений Ax = b:\n", np.linalg.tensorsolve(a, b)) 
+
+print("Метод наименьших квадратов:\n", np.linalg.lstsq(a, b))
+
+print("Обратная матрица:\n", np.linalg.inv(a))
