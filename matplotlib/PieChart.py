f9ac736501e479261e907a70d55a4ff3b2f5b444
haska_win10 <haska87@gmail.com>
2020-10-01 12:40:59 +0900

    add PieChart example

---
+++ matplotlib/PieChart.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt 
+plt.style.use('seaborn')
+
+slices = [59219, 55466, 47544, 36443, 35917, 31991, 27097, 23030, 20524, 18523, 18017, 7920, 7331, 7201, 5833]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java', 'Bash/Shell/PowerShell', 'C#', 'PHP', 'C++', 'TypeScript', 'C', 'Other(s):', 'Ruby', 'Go', 'Assembly']
+explode = [0, 0, 0, 0.1, 0]
+
+plt.pie(slices[:5], labels=labels[:5], explode=explode, shadow=True,
+        startangle=90, wedgeprops={'edgecolor':'black'}, autopct='%1.1f%%')
+
+plt.tight_layout()
+plt.show()
